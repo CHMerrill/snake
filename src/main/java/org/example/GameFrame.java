a3b73db9ee87d8285387558e49ee24870293c6e9
CHMerrill <115734377+CHMerrill@users.noreply.github.com>
2023-02-07 13:55:56 -0600

    foundation files

--- src/main/java/org/example/GameFrame.java
+++ src/main/java/org/example/GameFrame.java
@@ -3,4 +3,17 @@ package org.example;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
+
+    GameFrame(){
+//        GamePanel panel = new GamePanel();
+//        this.add(panel);
+//        Short version below is acceptable
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
 }
